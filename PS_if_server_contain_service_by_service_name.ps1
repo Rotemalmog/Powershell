2c05d1a9bd52de690c3a4c6d3cacd4590952bf50
RotemAlmog <rotemalmog10@gmail.com>
2022-06-22 14:15:16 +0300

    PS_if_server_contain_service_by_service_name.ps1

---
+++ PS_if_server_contain_service_by_service_name.ps1
@@ -0,0 +1,21 @@
+﻿$list = @()
+$Servers = Get-ADComputer -Filter  {operatingSystem -like "windows server 20*"}
+
+foreach ($server in $Servers)
+{
+$service = null
+$obj = New-Object -TypeName PSobject
+$obj | Add-Member -MemberType NoteProperty -Name "Host Name" -Value $server.name
+$service = Get-Service -ComputerName $server.Name -Name #service name#
+
+ if ($service)
+  {
+    $obj | Add-Member -MemberType NoteProperty -Name "Service" -Value ("Contain")
+  }
+  else
+  {
+  $obj | Add-Member -MemberType NoteProperty -Name "Service" -Value ("Not Contain")
+  }
+  $list += $obj
+}
+$list | Export-Excel -Show
